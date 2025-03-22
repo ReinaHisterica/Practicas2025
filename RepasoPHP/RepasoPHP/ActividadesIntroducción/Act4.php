81acc4004f1d368692f1c8aa694952e96c582842
ReinaHisterica <ReinaHisterica>
2025-03-22 22:28:26 +0100

    Ejercicios de repaso 1.

---
+++ RepasoPHP/RepasoPHP/ActividadesIntroducción/Act4.php
@@ -0,0 +1,12 @@
+<?php
+    $num1 = 505;
+    $num2 = 114;
+
+    if ($num1 > $num2) {
+        echo "$num1 es mayor que $num2.";
+    } else if ($num1 < $num2) {
+        echo "$num1 es menor que $num2.";
+    } else {
+        echo "$num1 es igual a $num2.";
+    }
+?>
