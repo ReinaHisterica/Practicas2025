41d16e18b4fa9d4ffcc6bd724ff91c69f13f8582
vjimenez-indigenasdigitales <vjimenez@indigenasdigitales.com>
2025-05-06 16:11:34 +0200

    Creados directorios y archivos para hacer login.

--- Proyecto/sabores-de-inca/routes/web.php
+++ Proyecto/sabores-de-inca/routes/web.php
@@ -20,27 +20,31 @@ Route::get('/', function () {
     return view('welcome'); // No borrar. Esto es para cuando entre a la raíz del proyecto.
 });
 
-// Una ruta por cada tabla.
-Route::get('/accesibilidad', [AccesibilidadController::class, 'index']);
-Route::get('/accesibilidad/{id}', [AccesibilidadController::class, 'show']);
+// // Una ruta por cada tabla.
+// Route::get('/accesibilidad', [AccesibilidadController::class, 'index']);
+// Route::get('/accesibilidad/{id}', [AccesibilidadController::class, 'show']);
 
-Route::get('/idioma', [IdiomaController::class, 'index']);
-Route::get('/idioma/{id}', [IdiomaController::class, 'show']); // Ruta para mostrar un idioma a partir de un id.
+// Route::get('/idioma', [IdiomaController::class, 'index']);
+// Route::get('/idioma/{id}', [IdiomaController::class, 'show']); // Ruta para mostrar un idioma a partir de un id.
 
-Route::get('/accesibilidad-traduccion', [AccesibilidadTraduccionController::class, 'index']);
-Route::get('/accesibilidad-traduccion/{id}', [AccesibilidadTraduccionController::class, 'show']);
+// Route::get('/accesibilidad-traduccion', [AccesibilidadTraduccionController::class, 'index']);
+// Route::get('/accesibilidad-traduccion/{id}', [AccesibilidadTraduccionController::class, 'show']);
 
-Route::get('/tipo-cocina', [TipoCocinaController::class, 'index']);
-Route::get('/tipo-cocina/{id}', [TipoCocinaController::class, 'show']);
+// Route::get('/tipo-cocina', [TipoCocinaController::class, 'index']);
+// Route::get('/tipo-cocina/{id}', [TipoCocinaController::class, 'show']);
 
-Route::get('/tipo-cocina-traduccion', [TipoCocinaTraduccionController::class, 'index']);
-Route::get('/tipo-cocina-traduccion/{id}', [TipoCocinaTraduccionController::class, 'show']);
+// Route::get('/tipo-cocina-traduccion', [TipoCocinaTraduccionController::class, 'index']);
+// Route::get('/tipo-cocina-traduccion/{id}', [TipoCocinaTraduccionController::class, 'show']);
 
-Route::get('/restaurante', [RestauranteController::class, 'index']);
-Route::get('/restaurante/{id}', [RestauranteController::class, 'show']);
+// Route::get('/restaurante', [RestauranteController::class, 'index']);
+// Route::get('/restaurante/{id}', [RestauranteController::class, 'show']);
 
-Route::get('/restaurante-accesibilidad', [RestauranteAccesibilidadController::class, 'index']);
-Route::get('/restaurante-accesibilidad/{id}', [RestauranteAccesibilidadController::class, 'show']);
+// Route::get('/restaurante-accesibilidad', [RestauranteAccesibilidadController::class, 'index']);
+// Route::get('/restaurante-accesibilidad/{id}', [RestauranteAccesibilidadController::class, 'show']);
 
-Route::get('/restaurante-traduccion', [RestauranteTraduccionController::class, 'index']);
-Route::get('/restaurante-traduccion/{id}', [RestauranteTraduccionController::class, 'show']);
+// Route::get('/restaurante-traduccion', [RestauranteTraduccionController::class, 'index']);
+// Route::get('/restaurante-traduccion/{id}', [RestauranteTraduccionController::class, 'show']);
+
+// Route::get('/login', function () {
+//     return 'Página de login aún no implementada';
+// })->name('login');
