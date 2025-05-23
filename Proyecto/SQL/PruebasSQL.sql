31be8fa0fea564263e6f3ed56d6d3441c73558e3
VirgiJM <virgijm@gmail.com>
2025-05-23 14:01:22 +0200

    Añadida consulta para obtener los servicios de un restaurante a partir de su id (e idioma).

--- Proyecto/SQL/PruebasSQL.sql
+++ Proyecto/SQL/PruebasSQL.sql
@@ -6,9 +6,8 @@ JOIN Idioma i ON i.idIdioma = at.fk_idIdioma WHERE i.CodigoIdioma = "es";
 SELECT at.Nombre, a.idAccesibilidad FROM accesibilidad_traduccion at 
 JOIN accesibilidad a ON a.idAccesibilidad = at.fk_idAccesibilidad
 JOIN Idioma i ON i.idIdioma = at.fk_idIdioma 
-JOIN tipo_cocina_traduccion tct ON tct.fk_idIdioma = i.idIdioma 
-JOIN tipo_cocina tc ON tc.idTipoCocina = tct.fk_idTipoCocina
-JOIN restaurante r ON tc.idTipoCocina = r.fk_idTipoCocina
+JOIN restaurante_accesibilidad ra ON a.idAccesibilidad = ra.fk_idAccesibilidad
+JOIN restaurante r ON r.idRestaurante = ra.fk_idRestaurante
 WHERE i.CodigoIdioma = "es" AND r.idRestaurante = 1;
 
 -- Ver los tipos de cocina y su nombre (Idioma ESP)
